76bd21edabac25423912ccde4422c04e56b19f0b
sai babu <saibabu@sais-MacBook-Air-3.local>
2024-02-21 15:06:34 +0530

    ADD.Simple CSS for Footer is done

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import './FooterComponent.css'
 function FooterComponent() {
   return (
-    <div>FooterComponent</div>
+    <div className='footer'>
+      <div className="footer-content">
+      <div className='footer-about'>
+        <div className='footer-about-title'>ABOUT</div>
+      <div>Contact Us</div>
+      <div>About Us</div>
+<div>Careers</div>
+      </div>
+<div className='footer-help'>
+  <div className='footer-help-title'>HELP</div>
+<div>Payments</div>
+<div>Shipping</div>
+<div>Cancellation & Returns</div>
+<div>FAQ</div>
+<div>Report Infringement</div>
+</div>
+<div className='footer-policy'>
+  <div className='footer-policy-title'>CONSUMER POLICY</div>
+<div>Cancellation & Returns</div>
+<div>Terms Of Use</div>
+<div>Security</div>
+<div>Privacy</div>
+<div>Sitemap</div>
+<div>Grievance Redressal</div>
+<div>EPR Compliance</div>
+</div>      
+    </div>
+    </div>
   )
 }
 
