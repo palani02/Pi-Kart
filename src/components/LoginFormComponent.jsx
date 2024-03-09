1c0379c9dc1de32c0110670cbac98e85cdaff3cd
sai babu <saibabu@sais-MacBook-Air-3.local>
2024-03-09 22:18:41 +0530

    UPDATE.updated the changes for loginFormComponent

--- src/components/LoginFormComponent.jsx
+++ src/components/LoginFormComponent.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Link,useNavigate} from 'react-router-dom'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import {auth} from '../config/fire'
 import './LoginFormComponent.css'
-function LoginForm() {
+function LoginFormComponent() {
+  const navigate = useNavigate();
+  const[values,setValues] = useState({
+    email:"",
+    pass:"",
+  });
+  const[errorMsg,setErrorMsg] = useState("");
+  const[submitButtonDisabled,setSubmitButtonDisabled] = useState(false);
+  const handleSubmission=()=>{
+    if(!values.email || !values.pass)
+    {
+      setErrorMsg("Fill all fields");
+      return;
+    }
+    setErrorMsg("");
+    setSubmitButtonDisabled(true);
+    signInWithEmailAndPassword(auth,values.email,values.pass)
+    .then(async(res)=>{
+      setSubmitButtonDisabled(false)
+      navigate("/");
+    })
+    .catch((err)=>{
+      setSubmitButtonDisabled(false);
+      setErrorMsg(err.message)
+    })
+  }
   return (
     <div>
         <form className='login-form'>
           <div className="login-content">
           <div className='login-title'>Login Credential</div>
             <label>
-                UserName
-                <input type='text'/>
+                Email
+                <input type='email' onChange={(event)=>setValues((prev)=>({...prev,email:event.target.value}))} placeholder='Enter Your Email'/>
             </label>
             <label>
                 Password
-                <input type='password'/>
+                <input type='password' onChange={(event)=>setValues((prev)=>({...prev,pass:event.target.value}))} placeholder='Enter your password'/>
             </label>
+            <div>
+              <b>{errorMsg}</b>
+            </div>
             <label>
-               <button type="submit">Login</button>
+               <button disabled={submitButtonDisabled} onClick={handleSubmission} type="submit">Login</button>
             </label>
             </div>
         </form>
@@ -23,4 +54,4 @@ function LoginForm() {
   )
 }
 
-export default LoginForm
+export default LoginFormComponent
